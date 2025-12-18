d87e63a04c8b5ae26beb229c575b8b798c619218
bala-amypo <bala@amypo.in>
2025-12-18 04:37:15 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -7,7 +7,7 @@ import com.example.demo.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService{
-       @Autowired StudentRepository student;
+       @Autowired StudentRepo student;
        @Override
   public StudentEntity postData(StudentEntity stu){
     return student.save(stu);
