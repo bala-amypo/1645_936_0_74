1e66bf6af9299e381f7073a23c5630579280dccb
bala-amypo <bala@amypo.in>
2025-12-18 08:37:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,11 +1,13 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.
-import jakarta.persistence.
-import jakarta.persistence.
-import jakarta.persistence.
-import jakarta.persistence.
-import jakarta.persistence.
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 
 @Entity
 public class ValidationEntity{
@@ -59,7 +61,7 @@ public class ValidationEntity{
     public ValidationEntity(long id,@NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
                     @Email(message="Email is not Valid") String email,
                     @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message="Password should not be empty") String password,
-                    @Max(30) @Positive(message="Age must be a positive number" int age){
+                    @Max(30) @Positive(message="Age must be a positive number" int age)){
 
         this.id=id;
         this.username=username;
