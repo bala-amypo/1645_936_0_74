2c5af1eae2436e499e857f6ff6a2b506d93d3850
bala-amypo <bala@amypo.in>
2025-12-18 08:22:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -23,4 +23,45 @@ public class ValidationEntity{
     @Max(30)
     private int age;
 
+    public void setId(Long id){
+        this.id=id;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public Date getCreated(){
+        return created;
+    }
+
+    public StudentEntity(){};
+    public StudentEntity(Integer id, String name, String username, String password, Date created){
+        this.id=id;
+        this.name=name;
+        this.username=username;
+        this.password=password;
+        this.created=created;
+    }
+
 }
