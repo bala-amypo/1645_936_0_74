fdad03e3b2ad55578eba452f0f6fa7db605e4989
bala-amypo <bala@amypo.in>
2025-12-19 05:55:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,74 +1,36 @@
-package com.example.demo.entity;
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+// import java.util.List;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import jakarta.validation.Valid;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Positive; 
 
-@Entity
-public class ValidationEntity{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotNull
-    @Size(min=2, max=10, message="must be 2 to 10 character")
-    private String username;
-    @Email(message = "Email is not valid")
-    private String email;
-    @Size(min=2, max=6, message="must be 2 to 6 character")
-    @NotNull(message="Password is mandatory")
-    private String password;
-    @Max(30)
-    private int age;
-
-    public void setId(Long id){
-        this.id=id;
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setAge(int age){
-        this.age=age;
+@RestController
+public class ValidationController{
+    @Autowired ValidationService ser;
+    @PostMapping("/post1")
+    public ValidationEntity sendData(@Valid @RequestBody ValidationEntity stu){
+        return ser.postData(stu);
     }
 
-    public Long getId(){
-        return id;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public int getAge(){
-        return age;
-    }
+    // @GetMapping("/get")
+    // public List<StudentEntity> getVal(){
+    //     return ser.getAllData();
+    // }
 
-    public ValidationEntity(){};
-    public ValidationEntity(Long id,@NotNull @Size(min=2,max=10,message="must be 2 to 10 character") String username,
-                    @Email(message="Email is not Valid") String email,
-                    @Size(min=2,max=10,message="must be 2 to 10 character") @NotNull(message="Password should not be empty") String password,
-                    @Max(30) @Positive(message="Age must be a positive number") int age){
-
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
+    // @DeleteMapping("/delete{id}")
+    // public String deleteValue(@PathVariable int id){
+    //     return ser.deleteData(id);
+    // }
+    @GetMapping("/get1{id}")
+    public ValidationEntity getValue(@PathVariable Long id){
+        return ser.getData(id);
     }
-
 }
