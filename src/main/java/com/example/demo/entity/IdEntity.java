b690553cb1fca8332aca0c03f1146da0f50e24e6
bala-amypo <bala@amypo.in>
2025-12-19 09:35:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/IdEntity.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
