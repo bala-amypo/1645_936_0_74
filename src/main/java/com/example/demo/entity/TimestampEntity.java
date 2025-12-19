1b4e75e37b993ba31c441ead1f85db2518710684
bala-amypo <bala@amypo.in>
2025-12-19 08:53:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimestampEntity.java
+++ src/main/java/com/example/demo/entity/TimestampEntity.java
@@ -27,5 +27,10 @@ public class TimestampEntity{
     private String email;
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
-    
+    @PrePersist
+    public void Oncreate(){
+        LocalDateTime now= new LocalDateTime();
+        this.cretedAt=now;
+        t
+    }
 }
